686c20144a936f24dd4d5baa8a0de66a04c674e7
muyanbiao <muyanbiao@xunlei.com>
2019-12-24 15:35:15 +0800

    1. 去掉SceneDelegate

--- ArchitureTemplate/ViewController.m
+++ ArchitureTemplate/ViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 100, 300)];
+    label.text = @"Hello iOS12 under Xcode11";
+    [self.view addSubview:label];
+    label.center = self.view.center;
 }
 
 
